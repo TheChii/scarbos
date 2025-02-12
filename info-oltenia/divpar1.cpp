49d36025ecda2895287c1fce6a8f5f9caccc117d
Chiriac Theodor <49607766+TheChii@users.noreply.github.com>
2025-02-12 20:40:52 +0200

    Create divpar1.cpp

---
+++ info-oltenia/divpar1.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+using namespace std;
+
+ifstream fin("divpar.in");
+ofstream fout("divpar.out");
+
+int main(){
+	int s=0;
+	int n; fin>>n;
+	
+	int x;
+	for(int i=0;i<n;i++){
+		fin>>x;
+		
+		int div = 1;
+		for(int d=2;d<=x;d++){
+			if(x%d==0) div++;
+		}
+		
+		if(div%2==0) s++;
+	}
+	
+	fout<<s;
+}
